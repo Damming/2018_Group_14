ea4fe02a5fa7ac2532a1269e56604e8ab5f83886
Daming <1556216184@qq.com>
2018-10-03 13:59:04 +1300

    tested Q3

--- Ass2_Q3/backup.sh
+++ Ass2_Q3/backup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Update Ubuntu & Install essential tools
 sudo apt-get update
 
@@ -21,49 +23,14 @@ sudo sed -i 's|md5|trust|' /etc/postgresql/10/main/pg_hba.conf
 sudo service postgresql restart
 
 # ------------------ backup -------------------
-# Manually backup 
 mkdir backup
-pg_dump -F c -f /home/ubuntu/backup/gis_backup.dmp  -C -E  UTF8 -h %actual_ip% -p 5432 -U postgres gis
+echo 'logs'>backup_log
 
-# Automatically backup
-# create a file in ~/ called auto_back.sh, put the following lines in it
-#
-# #! /bin/bash
-# pg_dump -F c -f /home/ubuntu/backup/gis_backup.dmp  -C -E  UTF8 -h %actual_ip% -p 5432 -U postgres gis
-# echo "backup finished" >> /home/ubuntu/backup_log
-#
-# to automate it, put the following line in /etc/crontab before '#' (backup every 15 minutes)
-# */5 * * * * ubuntu bash /home/ubuntu/auto_back.sh
+echo "#!/bin/bash
+pg_dump -F c -f /home/ubuntu/backup/gis_backup.dmp  -C -E  UTF8 -h $1 -p 5432 -U postgres gis
+echo "backup finished" >> /home/ubuntu/backup_log">auto_back.sh
 
-# ------------------ restore -------------------
-# Create the PostGIS Instance
-export PGPASSWORD=postgres_007%
-HOSTNAME=localhost
-psql -U postgres -h $HOSTNAME -c "CREATE DATABASE gis ENCODING 'UTF-8' LC_COLLATE 'en_GB.utf8' LC_CTYPE 'en_GB.utf8' TEMPLATE template0"
+# backup every 15 minutes
+echo '*/15 * * * * ubuntu bash /home/ubuntu/auto_back.sh' | sudo tee -a /etc/crontab
 
-# Create the postgis and hstore extensions
-psql -U postgres -h $HOSTNAME -c "\connect gis"
-psql -U postgres -h $HOSTNAME -d gis -c "CREATE EXTENSION postgis"
-psql -U postgres -h $HOSTNAME -d gis -c "CREATE EXTENSION hstore"
 
-# Add a user and grant access to gis DB
-psql -U postgres -c "create user ubuntu;grant all privileges on database gis to ubuntu;"
-
-# Enabling remote access to PostgreSQL
-sudo sed -i "10i\host all all 0.0.0.0/0 trust" /etc/postgresql/10/main/pg_hba.conf
-sudo sed -i "58i\listen_addresses = '*'" /etc/postgresql/10/main/postgresql.conf
-sudo /etc/init.d/postgresql restart
-
-# Tuning the database
-sudo echo 'shared_buffers = 128MB' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'min_wal_size = 80MB' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'max_wal_size = 1GB' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'work_mem = 4MB' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'maintenance_work_mem= 64MB' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'autovacuum = off' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo echo 'fsync = off' | sudo tee -a /etc/postgresql/10/main/postgresql.conf
-sudo /etc/init.d/postgresql stop
-sudo /etc/init.d/postgresql start
-
-# Restore
-pg_dump -F c -f ~/backup/gis_backup.dmp  -C -E  UTF8 -h %actual_ip% -p 5432 -U postgres gis
